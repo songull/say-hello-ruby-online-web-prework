19f848c1547fd2e44908300da7b65a0a681d6e88
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-19 01:32:28 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,4 @@
-def say_hello(name = "kent beck")
-  puts "Hello #{name}!"
+def say_hello(name, language="Ruby")
+  puts "Hello #{name}! We heard you are a great #{language}."
 end
+say_hello("kent back", "ruby programmer")
